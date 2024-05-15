7ba59d85f63f2cfd6d8335a40dd992ded04f1c0f
Fernando De Leon Gil <gasnare@hotmail.com>
2024-05-15 10:11:20 -0500

    Se agregan los links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,13 +10,27 @@ export const Footer = () => {
       <footer className="footer-container">
         <div className="footer-mancha" />
         <div className="footer-social">
-          <img src={Facebook} alt="Icono de la red social Facebook" />
-          <img src={Youtube} alt="Icono de la red social Youtube" />
-          <img src={Instagram} alt="Icono de la red social Instagram" />
-          <img
-            src={Spotify}
-            alt="Icono de la plataforma de streaming Spotify"
-          />
+          <a target="__blank" href="https://www.facebook.com/nxtwaaave/">
+            <img src={Facebook} alt="Icono de la red social Facebook" />
+          </a>
+          <a
+            target="__blank"
+            href="https://www.youtube.com/channel/UCb3_ZEgqrOEyqSPdrgjBEXQ"
+          >
+            <img src={Youtube} alt="Icono de la red social Youtube" />
+          </a>
+          <a target="__blank" href="https://www.instagram.com/nxtwaaave/?hl=en">
+            <img src={Instagram} alt="Icono de la red social Instagram" />
+          </a>
+          <a
+            target="__blank"
+            href="https://open.spotify.com/intl-es/artist/3icg8QXxKDihHvzfoC1Vpm?si=WlJpJdKCSQS9Wadv28GC7Q"
+          >
+            <img
+              src={Spotify}
+              alt="Icono de la plataforma de streaming Spotify"
+            />
+          </a>
         </div>
       </footer>
     </>
