e606ecb8f6933797a5dfa16a05ed352955d66ee3
Fernando De Leon Gil <gasnare@hotmail.com>
2024-05-17 11:33:43 -0500

    Se elimina código no necesario y actualización de link diferentes plataformas

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -28,42 +28,11 @@ export const Banner = () => {
           className="banner-presave"
           style={{ opacity: active ? 1 : 0, transition: transition }}
         >
-          <a
-            href="https://youtu.be/M8v_WA0BZao?si=X2VijxjVsOPNEX3R"
-            target="__blank"
-          >
-            <span />
-            Ver video
+          <a href="https://bfan.link/inhala-exhala" target="__blank">
+            ¡Disponible aquí!
           </a>
         </div>
       </div>
     </section>
   );
-  // return (
-  //   <section className="banner-section">
-  //     <div className="banner-container">
-  //       <div
-  //         className="banner-title"
-  //         style={{ opacity: active ? 1 : 0, transition: transition }}
-  //       >
-  //         <img
-  //           src={InhalaExhalaImage}
-  //           alt="Imagen con texto de Inhala Exhala"
-  //         />
-  //       </div>
-  //       <div
-  //         className="banner-presave"
-  //         style={{ opacity: active ? 1 : 0, transition: transition }}
-  //       >
-  //         <a
-  //           href="https://youtu.be/M8v_WA0BZao?si=X2VijxjVsOPNEX3R"
-  //           target="__blank"
-  //         >
-  //           <span />
-  //           Ver video
-  //         </a>
-  //       </div>
-  //     </div>
-  //   </section>
-  // );
 };
