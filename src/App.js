271ea34ec4a82c58ec5a47a97a998d90ef3135ab
Fernando De Leon Gil <gasnare@hotmail.com>
2024-06-21 14:52:14 -0500

    Ajuste de cambio de imagenes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import { Banner } from "./components/Banner";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { SectionInfo } from "./components/SectionInfo";
+import Loading from "./components/loading";
+
+import PerfectaPazImage from "./assets/banner/banner-perfecta-paz.png";
+import WeAreTheRevolutionImage from "./assets/banner/banner-we-are-the-revolution.png";
+
 import "./styles/main.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Banner />
-      <SectionInfo />
-      <Band />
-      <Footer />
-    </div>
+    <Loading
+      children={
+        <div>
+          <Header />
+          <Banner />
+          <SectionInfo />
+          <Band />
+          <Footer />
+        </div>
+      }
+      imageUrls={[PerfectaPazImage, WeAreTheRevolutionImage]}
+    />
   );
 }
 
