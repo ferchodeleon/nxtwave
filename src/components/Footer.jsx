bfea360dc2a76311e02f51f39c36a2d6cbbcb991
Fernando De Leon Gil <gasnare@hotmail.com>
2024-07-04 16:44:16 -0500

    Se agrega la bala que explota y ancho alto en imagenes de forma implicita

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import Facebook from "../assets/icons/facebook.svg";
 import Youtube from "../assets/icons/youtube.svg";
 import Instagram from "../assets/icons/instagram.svg";
@@ -5,28 +7,57 @@ import Spotify from "../assets/icons/spotify.svg";
 import "../styles/footer.css";
 
 export const Footer = () => {
+  const [exploded, setExploded] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setExploded((prev) => !prev);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <footer className="footer-container">
-        <div className="footer-mancha" />
+        <div
+          className={`footer-mancha ${exploded ? "new-mancha" : "exploding"}`}
+        />
         <div className="footer-social">
           <a target="__blank" href="https://www.facebook.com/nxtwaaave/">
-            <img src={Facebook} alt="Icono de la red social Facebook" />
+            <img
+              height="100%"
+              width="100%"
+              src={Facebook}
+              alt="Icono de la red social Facebook"
+            />
           </a>
           <a
             target="__blank"
             href="https://www.youtube.com/channel/UCb3_ZEgqrOEyqSPdrgjBEXQ"
           >
-            <img src={Youtube} alt="Icono de la red social Youtube" />
+            <img
+              height="100%"
+              width="100%"
+              src={Youtube}
+              alt="Icono de la red social Youtube"
+            />
           </a>
           <a target="__blank" href="https://www.instagram.com/nxtwaaave/?hl=en">
-            <img src={Instagram} alt="Icono de la red social Instagram" />
+            <img
+              height="100%"
+              width="100%"
+              src={Instagram}
+              alt="Icono de la red social Instagram"
+            />
           </a>
           <a
             target="__blank"
             href="https://open.spotify.com/intl-es/artist/3icg8QXxKDihHvzfoC1Vpm?si=WlJpJdKCSQS9Wadv28GC7Q"
           >
             <img
+              height="100%"
+              width="100%"
               src={Spotify}
               alt="Icono de la plataforma de streaming Spotify"
             />
