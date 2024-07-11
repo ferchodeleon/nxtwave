ce5e2dd1bffa96c333e4b3f1fc890c98d31bc977
Fernando De Leon Gil <gasnare@hotmail.com>
2024-07-11 09:08:36 -0500

    Ajuste menor

--- src/components/SectionVideo.jsx
+++ src/components/SectionVideo.jsx
@@ -19,9 +19,9 @@ export const SectionVideo = () => {
           <div className="sectionVideo-video-container" ref={videoContainerRef}>
             {isVisible ? (
               <iframe
+                title="YouTube video player sing 'Perfecta Paz'"
                 className="sectionVideo-video"
                 src="https://www.youtube.com/embed/dGhfwnpzzCg?si=NMFkR7W2VDGQMUdv"
-                title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
@@ -31,8 +31,6 @@ export const SectionVideo = () => {
               <div />
             )}
             <img
-              height="100%"
-              width="100%"
               className="sectionVideo-text"
               src={Cenefa}
               alt='imagen con el texto "Dame de tu paz tu perfecta paz mi confianza en ti permanece"'
